de32a70c043342b7f7feacbc3c6f3a1bc949562e
ithashedi <hello@hashedi.com>
2021-10-08 23:16:29 +0100

    init

--- scripts/index.js
+++ scripts/index.js
@@ -1,94 +1,89 @@
-//naming the elements from the DOM
-
-const cancelBtn = document.getElementById('btn-cancel');
-const form = document.querySelector('.search-form');
 const searchBox = document.querySelector('.search-box');
-// the searched value inputed by the user
-var inputValue = searchBox.value;
-// getting ride of the white spaces
-var searchQuery = inputValue.trim();
 
-const anotherSearchBtn = `<button class= another-search-btn onclick = "window.location.reload();"> Search Again</button> `
-const foundResults = `The search results of  ${searchQuery} ${anotherSearchBtn}`
 
-//hiding the cancel button (icon) when there is no text and showing it when there is text
-searchBoxCheck = () => {
+//adding an event  listener to the search box which will hide and show the cancel button
+searchBox.addEventListener('keydown', (event) => {
+    const cancelBtn = document.getElementById('btn-cancel');
     if (searchBox.value.length > 0) {
         cancelBtn.style.display = "block"
     } else {cancelBtn.style.display = "none"};
-    
-}
-
-
-//adding an event  listener to the search box whihc will hide and show the cancel button
-searchBox.addEventListener('keydown', (event) => {
-    searchBoxCheck();
   });
 
-
-//when cancel is clicked input should clear the search box
-function clearSearchBox() {
+  function clearSearchBox() {
     document.querySelector('.search-box').value = '';
 }
 
-
 async function handleSubmit(event) {
-  //preventing the reload the page
-  event.preventDefault();
-  // getting the searched results from the API calling  and storing them in the variable results
-  try {
+    event.preventDefault();
+    const inputValue = document.querySelector('.search-box').value;
+    const searchQuery = inputValue.trim();
+    
+    document.querySelector('.search-container').style.height = "65vh";
+
+    const searchResults = document.querySelector('.search-results');
+    searchResults.innerHTML = '';
+  
+    const spinner = document.querySelector('.js-spinner');
+    spinner.classList.remove('hidden');
+  
+    try {
       const results = await searchWikipedia(searchQuery);
-      document.querySelector('.search-container').style.height = "70vh";
-      document.querySelector('.found-results-text').style.display="flex";
-      document.querySelector('.found-results-text').style.justifyContent = "space-between";
-      document.querySelector('.search-container').style.display="none";
-      document.querySelector('.found-results-text').innerHTML='';
-      document.querySelector('.found-results-text').innerHTML= foundResults;
-      displaySearchResults(results);
+      
+      //throwing an error when no results are returned
+      if (results.query.searchinfo.totalhits === 0) {
+        alert(`No results found of ${searchQuery}. Try different keywords`);
+        return;
+      }
+      displayResults(results);
     } catch (err) {
       console.log(err);
-      alert('Failed to search wikipedia for', searchQuery);
+      alert('Failed to search wikipedia');
+    } finally {
+      spinner.classList.add('hidden');
     }
-}
-
-// calling the API with the search query (searchQuery) and return the response in json file 
-async function searchWikipedia(searchQuery) {
+  }
+  
+  async function searchWikipedia(searchQuery) {
     const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
-    // storing the response
     const response = await fetch(endpoint);
-    //checking for errors
     if (!response.ok) {
       throw Error(response.statusText);
     }
-    // returning the response in json format
     const json = await response.json();
     return json;
   }
-
-
-  function displaySearchResults(results) {
-    // Get a reference to the `.js-search-results` element
-const searchResults = document.querySelector('.results-container');
-
-// Iterate over the `search` array. Each nested object in the array can be
-// accessed through the `result` parameter
-results.query.search.forEach(result => {
-  const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
-
-  // Append the search result to the DOM
-  searchResults.insertAdjacentHTML(
-    'beforeend',
-    `<div class="result-item">
-      <h3 class="result-title">
-        <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
-      </h3>
-      <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
-      <span class="result-snippet">${result.snippet}</span><br>
-    </div>`
-  );
-});
-}
-
   
-  //adding the event listener to the form when submitted
-form.addEventListener('submit', handleSubmit);
+  function displayResults(results) {
+    
+    const searchResults = document.querySelector('.search-results');
+    
+    const inputValue = document.querySelector('.search-box').value;
+    document.querySelector('.found-results-text').innerHTML= `The search results of  ${inputValue}`;
+    document.querySelector('.found-results-text').style.display="block";
+
+    results.query.search.forEach(result => {
+      const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+  
+      searchResults.insertAdjacentHTML(
+        'beforeend',
+        `<div class="result-item">
+          <h3 class="result-title">
+            <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+          </h3>
+          <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+          <span class="result-snippet">${result.snippet}</span><br>
+        </div>`
+      );
+    });
+  }
+  
+  
+  function textResults () {
+    const inputValue = document.querySelector('.search-box').value;
+    document.querySelector('.found-results-text').innerHTML= `The search results of  ${inputValue}`;
+  }
+
+   
+  const form = document.querySelector('.search-form');
+  form.addEventListener('submit', handleSubmit);
+  
